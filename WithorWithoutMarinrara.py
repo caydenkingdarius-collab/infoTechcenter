52c93526643954ecfa3e17b4ed400467a720e43f
Johnson <caydenkingdarius@gmail.com>
2026-01-14 12:21:47 -0500

    While loop ... 95% stable doing Research

--- WithorWithoutMarinrara.py
+++ WithorWithoutMarinrara.py
@@ -1,5 +1,24 @@
 #Welcome Branch
 
+# Libraries imported here
+import sys
+import time
+
 print("\nWelcome Branch - Developer: Cayden Johnson")
 print("\nWelcome to infotech center V.1.0")
-print("\nInfoTechCenter is Booting Up...")
+
+
+x = 0
+ellipsis = 0
+
+while x != 20:
+    x += 1
+    ellipsisMessage = ("InfoTechCenter OS Booting" + "."* ellipsis)
+    ellipsis += 1
+    sys. stdout.write("\r" + ellipsisMessage +"   ")
+    sys.stdout.flush()
+    time.sleep(.5)
+    if ellipsis == 4:
+        ellipsis = 0
+    if x == 20:
+        print("\nOperating system Booted up - Retina scammd - Acces Granted")  
