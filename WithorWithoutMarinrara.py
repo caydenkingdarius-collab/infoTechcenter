0191690c2c75d74c2a293b80ea4eabacf8a09703
Cayden Johnson <caydenkingdarius@gmail.com>
2026-01-16 13:36:35 -0500

    Update WithorWithoutMarinrara.py
    Added color Thanks AI

--- WithorWithoutMarinrara.py
+++ WithorWithoutMarinrara.py
@@ -1,24 +1,41 @@
-#Welcome Branch
-
+# Welcome Branch
 # Libraries imported here
 import sys
 import time
+import os
+
+# Enable ANSI escape codes on Windows
+if os.name == "nt":
+    os.system("")
+
+# ANSI color codes
+GREEN = "\033[92m"
+BLACK_BG = "\033[40m"
+RESET = "\033[0m"
+
+# Clear screen and set colors
+print(BLACK_BG + GREEN, end="")
 
 print("\nWelcome Branch - Developer: Cayden Johnson")
 print("\nWelcome to infotech center V.1.0")
-
 
 x = 0
 ellipsis = 0
 
 while x != 20:
     x += 1
-    ellipsisMessage = ("InfoTechCenter OS Booting" + "."* ellipsis)
+    ellipsisMessage = "InfoTechCenter OS Booting" + "." * ellipsis
     ellipsis += 1
-    sys. stdout.write("\r" + ellipsisMessage +"   ")
+
+    sys.stdout.write("\r" + ellipsisMessage + "   ")
     sys.stdout.flush()
-    time.sleep(.5)
+    time.sleep(0.5)
+
     if ellipsis == 4:
         ellipsis = 0
+
     if x == 20:
-        print("\nOperating system Booted up - Retina scammd - Acces Granted")  
+        print("\nOperating system Booted up - Retina scammed - Access Granted")
+
+# Reset colors at the end
+print(RESET)
